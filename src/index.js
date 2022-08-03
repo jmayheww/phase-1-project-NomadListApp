0545d018a269800ce5ef289eacb4c73e4475dc05
Joshua <joshsmayhew@gmail.com>
2022-08-03 19:13:41 +0700

    debug fetch request and generated iterable data object, create functions filterContinentData and createCountryList to generate lists of continents and countries that append to DOM viafunction createSelectorList, add Event Listener to selectContinent dropdown triggers country list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,89 @@
-// Variable Declarations
+// Variable Declarations and DOM Selectors
 const rootURL = "https://api.teleport.org/api";
 
+const continentAbbreviations = {
+  Africa: "AF",
+  Antarctica: "AN",
+  Asia: "AS",
+  Europe: "EU",
+  North_America: "NA",
+  Oceania: "OC",
+  South_America: "SA",
+};
+
+const dropdownContainer = document.querySelector("#dropdown-lists");
+const continentContainer = document.querySelector("#continent-selector");
+const selectContinent = document.querySelector("#continents");
+
+// DOM Interactions
 document.addEventListener("DOMContentLoaded", () => {
+  getAPIData("continents", filterContinentData);
+});
 
-    getData(continents);
-
-})
-
-function getAPIData(directory) {
-
-    fetch(rootURL + `/${directory}`)
-    .then(resp => resp.json())
-    .then(data => console.log(data));
+// Asynchronous Functions: Fetch Data from API
+function getAPIData(strDirectory, callbackFun) {
+  fetch(`${rootURL}/${strDirectory}/`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      callbackFun(data);
+    });
 }
 
+// Handle API Data
+
+function filterContinentData(apiData) {
+  const continentList = apiData._links["continent:items"].map(
+    (cont) => cont.name
+  );
+
+  createSelectorList(continentList, selectContinent);
+}
+
+function createCountryList(apiData) {
+  const countrySelectorContainer = document.createElement("div");
+  const selectorLabel = document.createElement("label");
+  const selectCountry = document.createElement("select");
+  const defaultOption = document.createElement("option");
+
+  defaultOption.innerHTML = "Choose a country";
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+
+  selectorLabel.setAttribute("for", "countries");
+  selectCountry.id = "countries";
+
+  dropdownContainer.append(countrySelectorContainer);
+  countrySelectorContainer.append(selectorLabel);
+  selectorLabel.append(selectCountry);
+  selectCountry.appendChild(defaultOption);
+
+
+  const countryList = apiData._links["country:items"].map(
+    (country) => country.name
+  );
+
+  createSelectorList(countryList, selectCountry);
+}
+
+// Create Select List for Continents
+
+function createSelectorList(data, parentEl) {
+  data.forEach((datum) => {
+    let option = document.createElement("option");
+
+    option.textContent = datum;
+
+    parentEl.append(option);
+  });
+}
+
+// Add Event Listeners
+
+selectContinent.addEventListener("change", (event) => {
+  const continentSelection = event.target.value;
+
+  //   "https://api.teleport.org/api/continents/geonames:AF/countries/"
+  const countryListURL = `continents/geonames:${continentAbbreviations[continentSelection]}/countries`;
+
+  getAPIData(countryListURL, createCountryList);
+});
