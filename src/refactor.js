fade00ac281bb631a65a74d1b7d1cbeed4aa38c7
Joshua <joshsmayhew@gmail.com>
2022-08-05 16:09:25 +0700

    refactor js

--- src/refactor.js
+++ src/refactor.js
@@ -5,26 +5,34 @@ const continentdirectory = `${rootURL}/continents`;
 // const countrydirectory = `${rootURL}/continents/geonames:?/countries`;
 // const citydirectory =
 
-
 const dropdownContainer = document.querySelector("#dropdown-lists");
 const continentContainer = document.querySelector("#continent-selector");
 const selectContinent = document.querySelector("#continents");
 
-
 // ------------------------DOM Interaction--------------------------------
 
 document.addEventListener("DOMContentLoaded", () => {
-    getAPIData(continentdirectory);
-  });
+  getAPIData(continentdirectory, "continent", renderContinentData);
+
+});
 
 // -------------------Asynchronous Functions-----------------------
 // ---- Fetch Data From External API--------
 
-function getAPIData(directory) {
-    fetch(`${directory}`)
-    .then(resp => resp.json())
+function getAPIData(directory, strTargetLocation, renderLocationFunction) {
+  fetch(`${directory}`)
+    .then((resp) => resp.json())
     .then((data) => {
 
-        console.log('data: ', data);
-    })
+      const locationDataObj = data["_links"][`${strTargetLocation}:items`];
+
+      renderLocationFunction(locationDataObj);
+    });
+}
+
+// ------------------------- Handle API Data -------------------------
+
+function renderContinentData(apiDataObj) {
+  const continentList = apiDataObj.map((continent) => continent.name);
+  console.log("continentList: ", continentList);
 }
