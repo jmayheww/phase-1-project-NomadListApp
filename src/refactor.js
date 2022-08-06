0688a93fcf0d9f69d1c78148052a2ecec62a8b12
Joshua <joshsmayhew@gmail.com>
2022-08-06 14:17:17 +0700

    refactor dom functions createAppendOptions, rename global variables, edit html dropdowns, add style tag in html head assign hidden to .hide class

--- src/refactor.js
+++ src/refactor.js
@@ -30,10 +30,10 @@ const cityDropDown = document.querySelector("#cities");
 document.addEventListener("DOMContentLoaded", () => {
   getAPIData(continentDirectory)
     .then((data) => {
-      const continentList = parseAPIData(data, 'continent')
-      renderLocationData(continentList, continentDropDown)
+      const continentList = parseAPIData(data, "continent");
+      renderLocationData(continentList, continentDropDown);
     })
-    .catch((err) => console.log('Error fetching continents: ', err.message))
+    .catch((err) => console.log("Error fetching continents: ", err.message));
 });
 
 // -------------------Asynchronous Functions-----------------------
@@ -68,174 +68,26 @@ function resetDropDownOptions(dropdown) {
   const defaultOption = dropdown.children[0];
   console.log("defaultOption: ", defaultOption);
   defaultOption.selected = true;
-  dropdown.innerHTML = '';
+  dropdown.innerHTML = "";
   dropdown.append(defaultOption);
 }
 
 function renderLocationData(list, dropDown) {
-  dropDown.parentElement.classlist.remove('hide');
+  console.log('dropDown: ', dropDown);
+  dropDown.parentElement.classList.remove("hide");
   createAppendOptions(list, dropDown);
 }
 
 // EVENT LISTENERS ----------------------------------------------
 
-
-
-
-/* Continents ---------------------------------------- */
-function renderContinentData(apiDataObj) {
-  const continentList = apiDataObj.map((continent) => continent.name);
-  console.log("continentList: ", continentList);
-
-  appendFixedContinents(continentList, selectContinent);
-}
-
-/* Countries ----------------------------------------*/
-function renderCountryData(apiDataObj) {
-  const countryList = apiDataObj.map((country) => country.name);
-  console.log("countryList: ", countryList);
-
-  toggleSelectorListVisibility(selectCountry);
-
-  handleDynamicOptions(countryList, selectCountry);
-
-  //   createDropdownDOMList(countryList, "country", "countries", selectCountry);
-}
-
-/* Cities --------------------------------------------*/
-function renderCityData(apiDataObj) {
-  const selectCity = document.createElement("select");
-
-  const citiesList = apiDataObj["_embedded"]["city:search-results"].map((x) => {
-    return x["_embedded"]["city:item"]["name"];
-  });
-
-  toggleSelectorListVisibility(selectCity);
-  handleDynamicOptions(citiesList, selectCity);
-}
-
-// renderCityData()
-
-// --------------------------DOM Manipulation-----------------------------------------
-
-// --------------------- Append Selector List Options ----------------------
-
-function toggleSelectorListVisibility(selectorList) {
-  return selectorList.hidden === true
-    ? selectorList.removeAttribute("hidden")
-    : true;
-}
-
-/* Creates new selector dropdowns in DOM */
-
-// function createDropdownDOMList(strDataArr, target, id, selectorListEl) {
-/* Check which dropdowns currently exist */
-
-//   const currentDropdownsByID = [];
-//   const currentDropdownSelectors = document
-//     .querySelectorAll(".selectors")
-//     .forEach((dropdown) => {
-//       currentDropdownsByID.push(dropdown.id);
-//     });
-//
-
-//   /* Determine whether or not dropdown is created
-//         if dropdown id does not currently exist, create new dropdown,
-//         otherwise refrain  */
-
-//   if (!currentDropdownsByID.includes(id)) {
-//   const selectorContainer = document.createElement("div");
-//   const selectorList = document.createElement("select");
-//   const selectorLabel = document.createElement("label");
-//   const defaultOption = document.createElement("option");
-
-//   defaultOption.innerHTML = `Choose a ${target}`;
-//   defaultOption.disabled = true;
-//   defaultOption.selected = true;
-
-//   selectorLabel.setAttribute("for", id);
-//   selectorListEl.id = id;
-//   selectorListEl.className = "selectors";
-
-//   dropdownContainer.append(selectorContainer);
-//   selectorContainer.append(selectorLabel);
-//   selectorLabel.append(selectorListEl);
-//   selectorListEl.appendChild(defaultOption);
-
-//   strDataArr.forEach((str) => {
-//     const option = document.createElement("option");
-//     option.innerText = str;
-
-//     selectorListEl.append(option);
-//   });
-// }
-
-/* Appends Options to Dropdown Selectors */
-
-function appendFixedContinents(strDataArr, parentEl) {
-  /*strDataArr must be clean location data, comprising geographical location names only */
-
-  strDataArr.forEach((str) => {
-    const option = document.createElement("option");
-    option.innerText = str;
-
-    parentEl.append(option);
-  });
-}
-
-// function handleNewDropdowns() {
-//   const currentDropdownsByID = [];
-//   const currentDropdownSelectors = document
-//     .querySelectorAll(".selectors")
-//     .forEach((dropdown) => {
-//       currentDropdownsByID.push(dropdown.id);
-//     });
-//
-
-//   /* Determine whether or not dropdown is created
-//         if dropdown id does not currently exist, create new dropdown,
-//         otherwise refrain  */
-
-//   if (!currentDropdownsByID.includes(id)) {
-//     createDropdownDOMList()
-//   }
-// }
-
-// ------------------------ Initialize Event Listeners -----------------------
-
-function continentListener() {
-  selectContinent.addEventListener("change", (event) => {
-    const continentSelection = event.target.value;
-    const countryListArr = `${countryDirectory}${continentAbbreviations[continentSelection]}/countries`;
-
-    return getAPIData(countryListArr, "country", renderCountryData);
-  });
-}
-
-// function countryListener() {}
-
-function onChangeListener(selectorList) {
-  selectorList.addEventListener("change", (event) => {
-    const selectionValue = event.target.value;
-
-    if (selectorList === selectContinent) {
-      const countryListArr = `${countryDirectory}${continentAbbreviations[selectionValue]}/countries`;
-
-      return getAPIData(countryListArr, "country", renderCountryData);
-    } else if (selectorList === selectCountry) {
-      const cityListArr = `${cityDirectory}${selectionValue}`;
-      console.log("cityListArr: ", cityListArr);
-
-      return getAPIData(cityListArr, "city", renderCityData);
-    }
-  });
-}
-
-function handleDynamicOptions(strDataArr, parentEl) {
-  strDataArr.forEach((str) => {
-    const option = document.createElement("option");
-
-    option.innerText = str;
-    parentEl.append(option);
-  });
-}
+continentDropDown.addEventListener("change", (event) => {
+  const continentSelection = event.target.value;
+  const countryURLString = `${countryDirectory}${ISO_Map[continentSelection]}/countries`;
+  getAPIData(countryURLString)
+    .then((data) => {
+      const countryList = parseAPIData(data, "country");
+      resetDropDownOptions(countryDropDown);
+      renderLocationData(countryList, countryDropDown);
+    })
+    .catch((err) => console.log("Error fetching countries!: ", err.message));
+});
